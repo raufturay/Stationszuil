4cc6412fc44db3bcb4ba60eb7c3393f2fe64a86e
raufturay <raufturay@gmail.com>
2022-10-19 02:36:19 +0200

    moderator doesn't get added to table moderater if it already exists. moderatorid from the moderator that does the check gets added in table berichten.

--- module2.py
+++ module2.py
@@ -1,16 +1,25 @@
-import sqlite3
 import datetime
-conn = sqlite3.connect('stationszuil.db')
+import psycopg2
+conn = psycopg2.connect(
+   dbname="Stationszuil", user='postgres', password='rauf', host='localhost')
 c = conn.cursor()
 
+def data_entry(name, date, station, bericht, goedkeuring, keurdatum, keurtijd, id):
+    c.execute("INSERT INTO berichtenns (naam, datum, bericht, station,goedkeuring,goedkeuringdatum,goedkeuringtijd,moderatorid) VALUES(%s , %s,%s,%s,%s,%s,%s,%s)",(name, date, station, bericht,goedkeuring, keurdatum, keurtijd,id))
+    # c.execute(f"INSERT INTO berichtenns (naam, datum, bericht, station,goedkeuring,goedkeuringdatum,goedkeuringtijd) VALUES('{name}', '{date}', '{station}', '{bericht}','{goedkeuring}', '{keurdatum}', '{keurtijd}');")
 
-def data_entry(name, date, station, bericht, goedkeuring, keurdatum, keurtijd, moderatornaam, moderatoremail):
-    c.execute("INSERT INTO berichten (Naam, Datum, Station, Bericht) VALUES(?, ?,?,?)",
-              (name, date, station, bericht))
-    c.execute("INSERT INTO beoordeling (Goedkeuring,Datum,Tijd) VALUES(?, ?,?)",
-              (goedkeuring, keurdatum, keurtijd))
-    c.execute("INSERT INTO moderator (Naam, Emailadres) VALUES(?, ?)",
-              (moderatornaam, moderatoremail))
+
+
+
+
+    conn.commit()
+
+def data_entry2(name, date, station, bericht, goedkeuring, keurdatum, keurtijd ,id):
+    c.execute("INSERT INTO berichtenns (naam, datum, bericht, station,goedkeuring,goedkeuringdatum,goedkeuringtijd,moderatorid) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",(name, date, station, bericht, goedkeuring, keurdatum, keurtijd,id))
+
+
+        # c.execute(f"INSERT INTO berichtenns (naam, datum, bericht, station,goedkeuring,goedkeuringdatum,goedkeuringtijd) VALUES('{name}', '{date}', '{station}', '{bericht}','{goedkeuring}', '{keurdatum}', '{keurtijd}');")
+
 
     conn.commit()
 
@@ -20,13 +29,18 @@ file.close()
 
 file = open("module1.csv", "r")
 for line in range(1,lines,4):
+
     naam = file.readlines(line)
+    naam = naam[0].strip()
     print(naam)
     datum = file.readlines(line+1)
+    datum = datum[0].strip()
     print(datum)
     station = file.readlines(line+2)
+    station = station[0].strip()
     print(station)
     bericht = file.readlines(line+3)
+    bericht = bericht[0].strip()
     print(bericht)
     goedkeuring = input('keur je dit bericht goed: ')
     date = datetime.datetime.now()
@@ -34,6 +48,24 @@ for line in range(1,lines,4):
     keurtijd = date.time()
     moderatornaam = input('wat is je naam: ')
     moderatoremail = input('wat is je email: ')
-    data_entry(naam,datum,station,bericht,goedkeuring,keurdatum,keurtijd,moderatornaam,moderatoremail)
+    c.execute("SELECT email FROM moderatorns ")
+    moderators = c.fetchall()
+
+    for moderator in moderators:
+        if moderator[0] == moderatoremail:
+            c.execute("SELECT moderatorid FROM moderatorns WHERE email =  %s", [moderatoremail])
+            id = c.fetchall()
+            data_entry(naam, datum, station, bericht, goedkeuring, keurdatum, keurtijd, id[0])
+            print("1")
+            break
+    else:
+        c.execute("INSERT INTO moderatorns (naam, email) VALUES(%s,%s)", (moderatornaam, moderatoremail))
+
+        c.execute("SELECT MAX(moderatorid) FROM moderatorns ")
+        modid = c.fetchall()
+        id = modid[0]
+        id = id[0]
+        data_entry2(naam,datum,station,bericht,goedkeuring,keurdatum,keurtijd,id)
+        print("2")
 
 file.close()
